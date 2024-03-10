4b010b143f74254643aba2b615dc9c53d425abec
Alex Bedon <alexanderbedon58724222@gmail.com>
2024-03-10 12:01:11 -0500

    change CATALOGO

--- client/src/components/CatalogoSellos.jsx
+++ client/src/components/CatalogoSellos.jsx
@@ -9,6 +9,16 @@ import sellaViena from '../image/sellaViena.jpg';
 import selloNumeradora from '../image/selloNumeradora.jpg';
 import selloSeco from '../image/selloSeco.jpg';
 import selloMadera from '../image/selloMadera.jpg';
+import AlmohadillaDactilar from '../image/AlmohadillaDactilar.png';
+import AlmohadillaMadera from '../image/almohadillaMadera.jpg';
+import repuestoAlmohadilla from '../image/repuestoAlmohadilla.jpg';
+
+import jarroCervecero from '../image/jarroCervecero.jpg';
+import jarroEcuador from '../image/jarroEcuador.png';
+import jarroMagico from '../image/jarroMagico.jpg';
+import tintaPlastico from '../image/tintaPlastico.jpg';
+import tintaSello from '../image/tintaSello.jpg';
+import tintaTextil from '../image/tintaTextil.jpg';
 
 const CatalogoSellos = () => {
     return (
@@ -21,29 +31,7 @@ const CatalogoSellos = () => {
       alt="Foto grande"
     />
   </div>
-  <h1>SELLOS</h1>
-  <nav className="nav2">
-    <button className="toggle">
-      <i className="fa-solid fa-bars" />
-    </button>
-    <ul className="navMenu">
-      <li className="navMenuItem">
-        <a href="../HTML/almohadilla.html" className="navMenuLink navLink">
-          almohadillas
-        </a>
-      </li>
-      <li className="navMenuItem">
-        <a href="../HTML/tintas.html" className="navMenuLink navLink">
-          Tintas
-        </a>
-      </li>
-      <li className="navMenuItem">
-        <a href="../HTML/jarros.html" className="navMenuLink navLink">
-          JARROS
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <h1>PRODUCTOS</h1>
   <div className="carrito">
     <div>
       <img
@@ -248,6 +236,215 @@ const CatalogoSellos = () => {
             </p>
           </div>
         </div>
+      </div>
+     
+      <div className="carts">
+        <img src={AlmohadillaDactilar} alt="" />
+        <p className="title">
+        ALMOHADILLA DACTILAR
+          <br />
+        </p>
+        <p>
+          <span>8</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={10} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Una almohadilla de tinta especialmente diseñada para la toma de huellas dactilares,
+                 proporcionando un recubrimiento uniforme de tinta que permite una impresión clara y legible.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="carts">
+        <img src={AlmohadillaMadera} alt="" />
+        <p className="title">
+        ALMOHADILLA MADERA
+          <br />
+        </p>
+        <p>
+          <span>10</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={11} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Una almohadilla diseñada para aplicarse en sellos de madera, 
+                ofreciendo una base firme y uniforme para la tinta, garantizando impresiones limpias y precisas.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="carts">
+        <img src={repuestoAlmohadilla} alt="" />
+        <p className="title">
+        REPUESTO ALMOHADILLA
+          <br />
+        </p>
+        <p>
+          <span>5</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={12} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Una almohadilla de tinta diseñada para reemplazar la almohadilla agotada en sellos de caucho o polímero,
+                 permitiendo prolongar la vida útil del sello sin comprometer la calidad de las impresiones.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="carts">
+        <img src={tintaSello} alt="" />
+        <p className="title">
+        TINTA SELLO
+          <br />
+        </p>
+        <p>
+          <span>12</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={13} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Diseñada para aplicarse en sellos de caucho o polímero, proporcionando una impresión nítida y
+                duradera en una variedad de superficies.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="carts">
+        <img src={tintaPlastico} alt="" />
+        <p className="title">
+        TINTA PLASTICO
+          <br />
+        </p>
+        <p>
+          <span>18</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={14} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Es una tinta especialmente formulada para adherirse de manera
+                 duradera a superficies de plástico, ofreciendo resistencia a la abrasión y al lavado.
+            </p>
+          </div>
+        </div>
+        
+      </div>
+      <div className="carts">
+        <img src={tintaTextil} alt="" />
+        <p className="title">
+        TINTA TEXTIL
+          <br />
+        </p>
+        <p>
+          <span>15.50</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={15} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Formulada para adherirse de manera efectiva a tejidos, 
+                ofreciendo colores vibrantes y duraderos incluso después de múltiples lavados.
+            </p>
+          </div>
+        </div>
+        
+      </div>
+      <div className="carts">
+        <img src={jarroCervecero} alt="" />
+        <p className="title">
+        JARRO CERVECERO
+          <br />
+        </p>
+        <p>
+          <span>16</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={16} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Jarros cerveceros con el logotipo de una empresa, un diseño gráfico específico o 
+                incluso con imágenes fotográficas. La impresión se realiza con técnicas especializadas
+                en el material del jarro, lo que garantiza 
+                que el diseño sea duradero y resistente a los lavados.
+            </p>
+          </div>
+        </div>
+        
+      </div>
+      <div className="carts">
+        <img src={jarroEcuador} alt="" />
+        <p className="title">
+        JARRO CERAMICA
+          <br />
+        </p>
+        <p>
+          <span>16</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={17} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Se imprimime con un diseño tecnico
+                 Utilizando técnicas de impresión de sublimación se garantiza que el diseño se aplique de manera precisa y
+                  que la revelación del diseño sea sorprendente y atractiva.
+            </p>
+          </div>
+        </div>
+        
+      </div>
+      <div className="carts">
+        <img src={jarroMagico} alt="" />
+        <p className="title">
+        JARRO MAGICO
+          <br />
+        </p>
+        <p>
+          <span>10</span>$
+        </p>
+        <div className="overlay">
+          <button href="" data-id={18} className="btn-add-cart">
+            Agregar
+          </button>
+          <button className="description-btn">Descripción</button>
+          <div className="description-content">
+            <p>
+            Los jarros mágicos son una opción creativa para regalos promocionales, Se imprimime con un diseño oculto 
+                que solo se revela cuando se llena el jarro con líquido caliente.
+                 Utilizando técnicas de impresión de sublimación se garantiza que el diseño se aplique de manera precisa y
+                  que la revelación del diseño sea sorprendente y atractiva.
+            </p>
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
