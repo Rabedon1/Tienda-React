0f1840f203088e11888b01e1583ea933cbcc1ae3
Alex Bedon <alexanderbedon58724222@gmail.com>
2024-03-10 23:19:22 -0500

    create productos

--- client/src/components/Productos.jsx
+++ client/src/components/Productos.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
+import { useState } from 'react';
+import Axios from "axios";
+
+
 
 const Productos = () => {
+
+    const [producto,setProducto] = useState("");
+    const [cantidad,setCantidad] = useState(0);
+    const [precio,setPrecio] = useState(0);
+    const [categoria,setCategoria] = useState("");
+
+const registrar = ()=>{
+    Axios.post("http://localhost:5000/create",{
+        producto:producto,
+        cantidad:cantidad,
+        precio:precio,
+        categoria:categoria,
+
+
+    }).then(()=>{
+        alert("Producto Registrado");
+    });
+}
+
+
+
     return (
-        <div class="container">
+
+        <div className="Productos">
+            <div className="datos">
+                <label>Productos: <input 
+                onChange={(event)=>{
+                    setProducto(event.target.value);
+                }}
+                type="texto"/></label>
+                <label>Cantidad: <input 
+                onChange={(event)=>{
+                    setCantidad(event.target.value);
+                }}
+                type="number"/></label>
+                <label>Precio: <input 
+                onChange={(event)=>{
+                    setPrecio(event.target.value);
+                }}
+                type="number"/></label>
+                <label>Categoria: <input 
+                onChange={(event)=>{
+                    setCategoria(event.target.value);
+                }}
+                type="texto"/></label>
+                <button onClick={registrar}>Registrar Producto</button>
+
+
+            </div>
+        </div>
+       /*  <div class="container">
+            <h1>REGISTRO DE PRODUCTOS</h1>
             <table class="table">
                 <thead class="table-dark">
                     <tr>
@@ -37,10 +91,13 @@ const Productos = () => {
                     
                  </tbody>
             </table>
-            <button className="btn btn-success">Success</button>
-            
+            <div className="btn-group" role= "group" aria-label="Basic example">
+                <button type="button" className="btn btn-primary">Mostrar</button>
 
-        </div>
+
+            </div>
+        </div> */
+
     );
 };
 
